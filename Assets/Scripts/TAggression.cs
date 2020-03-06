032c6f8d05156439eb77312e38559b17c5f25479
Jonny191994 <31445472+Jonny191994@users.noreply.github.com>
2020-03-05 20:11:59 -0500

    Forest Project
    Forest with tiger patrolling

---
+++ Assets/Scripts/TAggression.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TAggression : MonoBehaviour
+{
+    Rigidbody rb;
+    public Transform player;
+    public float hungerForce = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 playerDirection = Vector3.Normalize(player.position - transform.position);
+
+        rb.AddForce(playerDirection * hungerForce);
+    }
+}
---
+++ Assets/Scripts/TMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public Transform[] points;
+    private int destPoint;
+    private NavMeshAgent agent;
+
+
+    // Start is called before the first frame update
+    void GoToNextPoint()
+    {
+        if (points.Length == 0)
+            return;
+        agent.destination = points[destPoint].position;
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+
+        GoToNextPoint();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GoToNextPoint();
+    }
+}
